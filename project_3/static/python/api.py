45aaacd6dbb6ae58dc4df99f65fa44a2719378bd
Anthony Costa <acosta1019@gmail.com>
2023-04-20 12:08:49 -0400

    Working on gitignore

--- project_3/static/python/api.py
+++
@@ -1,8 +0,0 @@
-#for covid act now api
-apiEndpoint = "https://api.covidactnow.org/v2/states.json"
-apiEndpoint_csv = "https://api.covidactnow.org/v2/states.csv"
-apiKey = "0c417904f50d4ee1ba49933bd4d12c08"
-
-#geoapify api
-base_url = "https://api.geoapify.com/v1/places"
-geoapify_key = "7226d552878646b39eb2195f30123ce4"
